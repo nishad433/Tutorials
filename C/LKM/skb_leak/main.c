04d2514db945b03f3668898e25a53a01c56a8b41
Muhammed Nishad Chengalan Chalipurath <mnishadc@juniper.net>
2023-09-12 19:16:29 +0000

    skb leak, update the module

--- C/LKM/skb_leak/main.c
+++ C/LKM/skb_leak/main.c
@@ -8,12 +8,20 @@
 #include <asm/uaccess.h>
 #include <slab.h>
 #include <linux/skbuff.h>
+#include <linux/netdevice.h>
+#include <net/sock.h>
+#include <linux/net.h>
+#include <linux/fs.h>
 
 int ncc_log_level = 0;
 int ncc_mod = 0;
 int ncc_cap_point = 0;
 int ncc_cap_type = 0;
 int ncc_skb_idx = 0;
+int ncc_skb_Cntr = 0;
+int ncc_dump_cmd = 0;
+
+int ncc_logged_pkt_cnt = 0;
 
 #if 1
 #define dprintf(x,...) do { if(ncc_log_level >= 1) pr_info(x, ##__VA_ARGS__); } while (0)
@@ -56,7 +64,9 @@ extern struct kmem_cache *skbuff_head_cache;
 #define MIST_MOD_PACE           0x3
 #define MIST_MOD_WL             0x4
 #define MIST_MOD_BLE            0x5
-#define MIST_MOD_LAST           0x6
+#define MIST_MOD_SKBUFF         0x6
+#define MIST_MOD_DEVNIT         0x7
+#define MIST_MOD_LAST           0x8
 */
 
 char *module_itoa[MIST_MOD_LAST] = {
@@ -66,7 +76,9 @@ char *module_itoa[MIST_MOD_LAST] = {
 	"MIST_PACE",
 	"MIST_WL",
 	"MIST_BLE",
-	"MIST_SKBUFF" "KERNEL"
+	"MIST_SKBUFF", 
+	"MIST_DEVNIT", 
+        "NA"
 };
 
 #define NCC_C_TYPE_NONE 0x0
@@ -85,9 +97,15 @@ char *ctype_itoa[NCC_C_TYPE_LAST] = {
 };
 
 typedef struct {
-	u64 mist_counters[MIST_MOD_LAST][MIST_CAP_LAST];
+	u64 cntr;
+	u64 bridged;
+	u64 host;
+} mist_t;
+
+typedef struct {
+	mist_t mist_counters[MIST_MOD_LAST][MIST_CAP_LAST];
 	u64 mist_cntr_err, mist_last_seen_err;
-	u64 mist_lastseen_counters[MIST_MOD_LAST];
+	mist_t mist_lastseen_counters[MIST_MOD_LAST];
 } ncc_stats_t;
 
 ncc_stats_t n_stats;
@@ -98,6 +116,7 @@ static int config_show(struct seq_file *m, void *arg) {
 	seq_printf(m, "CapPoint(C)=%d\n", ncc_cap_point);
 	seq_printf(m, "CaptureType(T)=%d(%s)\n", ncc_cap_type, ctype_itoa[ncc_cap_type]);
 	seq_printf(m, "SKBidx(I)=%d\n", ncc_skb_idx);
+	seq_printf(m, "SKBCntr(i)=%d\n", ncc_skb_Cntr);
 	return 0;
 }
 
@@ -129,6 +148,9 @@ static ssize_t config_wr(struct file *filp, const char __user * buf, size_t coun
 		case 'I':
 			ncc_skb_idx = (int)res;
 			break;
+		case 'i':
+			ncc_skb_Cntr = (int)res;
+			break;
 		case 'T':
 			ncc_cap_type = (int)res;
 			break;
@@ -138,27 +160,79 @@ static ssize_t config_wr(struct file *filp, const char __user * buf, size_t coun
 	return count;
 }
 
-void update_counters(uint16_t flags, uint16_t last_seen) {
+void update_counters(struct sk_buff *skb) {
+	uint16_t flags, last_seen;
 	int module, cap_point, module_last_seen;
+	if (!skb) {
+		return;
+	}
+	flags = skb->ncc_flags;
+	last_seen = skb->ncc_last_seen;
 	module = ((flags >> 12) & 0xf);
 	cap_point = (flags & 0xfff);
 	module_last_seen = (last_seen & 0xf);
 
 	if (module >= 0 && module < MIST_MOD_LAST && cap_point >= 0 && cap_point < MIST_CAP_LAST) {
-		n_stats.mist_counters[module][cap_point] += 1;
-		n_stats.mist_counters[module][0] += 1;
+		n_stats.mist_counters[module][cap_point].cntr += 1;
+		n_stats.mist_counters[module][0].cntr += 1;
+		if (skb->sk) {
+			n_stats.mist_counters[module][cap_point].host += 1;
+			n_stats.mist_counters[module][0].host += 1;
+		} else {
+			n_stats.mist_counters[module][cap_point].bridged += 1;
+			n_stats.mist_counters[module][0].bridged += 1;
+		}
 	} else {
 		n_stats.mist_cntr_err += 1;
 		dprintf("NCC[%s] module=%d cap_point=%d flags=%x\n", __func__, module, cap_point, flags);
 	}
 
 	if (module_last_seen >= 0 && module_last_seen < MIST_MOD_LAST) {
-		n_stats.mist_lastseen_counters[module_last_seen] += 1;
+		n_stats.mist_lastseen_counters[module_last_seen].cntr += 1;
+		if (skb->sk) {
+			n_stats.mist_lastseen_counters[module_last_seen].host += 1;
+		} else {
+			n_stats.mist_lastseen_counters[module_last_seen].bridged += 1;
+		}
 	} else {
 		n_stats.mist_last_seen_err += 1;
 		dprintf("NCC[%s] module=%d cap_point=%d flags=%x\n", __func__, module, cap_point, flags);
 	}
 
+}
+
+void decode_hex_pkt(struct seq_file *m, struct sk_buff *skb) {
+	seq_printf(m, "skb_len=%d data=%px dev=%s users=%d sk=%x cloned=%d\n", skb->len, skb->data, skb->dev ? skb->dev->name : "NA", skb->users, (skb->sk != NULL), skb->cloned);
+	if (skb->sk && skb->sk->sk_socket && skb->sk->sk_socket->file && skb->sk->sk_socket->file->f_owner.pid) {
+		seq_printf(m, "pid =%d\n", pid_nr(skb->sk->sk_socket->file->f_owner.pid));
+	}else{
+		if(skb->sk){
+			seq_printf(m, "sk=%px ", skb->sk);
+			if(skb->sk->sk_socket){
+				seq_printf(m, "socket=%px ", skb->sk->sk_socket);
+				if(skb->sk->sk_socket->file){
+					seq_printf(m, "file=%px ", skb->sk->sk_socket->file);
+					if(skb->sk->sk_socket->file->f_owner.pid){
+						seq_printf(m, "pid=%px ", skb->sk->sk_socket->file->f_owner.pid);
+					}
+				}
+			}
+		}
+		seq_printf(m, "\n");
+	}
+	if (skb->len > 14) {
+		int i;
+		for (i = 0; i < 100 && i < skb->len; i++) {
+			if (i > 0 && i % 16 == 0) {
+				seq_printf(m, "\n");
+			}
+			if (i % 16 == 0) {
+				seq_printf(m, "0x%4x\t\t", (int)(skb->data + i) & 0xffff);
+			}
+			seq_printf(m, "0x%02x ", *((char *)(((char *)skb->data) + i)));
+		}
+		seq_printf(m, "\n");
+	}
 }
 
 static void ncc_skb_dump(struct seq_file *m, struct sk_buff *skb, int *skbCount) {
@@ -195,10 +269,19 @@ static void ncc_skb_dump(struct seq_file *m, struct sk_buff *skb, int *skbCount)
 		break;
 	}
 
+	if (ncc_skb_Cntr > 0 && ncc_logged_pkt_cnt >= ncc_skb_Cntr) {
+		logSKB = 0;
+	}
+
 	if (logSKB) {
-		seq_printf(m, "%d] skb=%px len=%d module=%s cap_point=%d last_seen=%s\n", *skbCount, skb, skb->len, module_itoa[module], cap_point, module_itoa[module_last_seen]);
-	} else {
-		dddprintf("NCC[%s:%d]logSKB=%d,skbCount=%d ncc_cap_type=%d\n", __func__, __LINE__, logSKB, *skbCount, ncc_cap_type);
+		seq_printf(m, "\n%d] skb=%px len=%d module=%s cap_point=%d last_seen=%s\n", *skbCount, skb, skb->len, module_itoa[module], cap_point, module_itoa[module_last_seen]);
+	}
+
+	if (logSKB && ncc_dump_cmd == 2 && skb && (ncc_skb_Cntr == -1 || ((--ncc_skb_Cntr) > 0))) {
+		decode_hex_pkt(m, skb);
+	}
+	if (logSKB) {
+		++ncc_logged_pkt_cnt;
 	}
 }
 
@@ -212,7 +295,7 @@ static int dump_slab_page(struct seq_file *m, struct list_head *head, int obj_si
 		skb = (struct sk_buff *)page_address(page);
 		for (j = 0; j < objsPerSlab; j++) {
 			if (skb->ncc_alloced == SKB_MIST_MAGIC) {
-				update_counters(skb->ncc_flags, skb->ncc_last_seen);
+				update_counters(skb);
 				if (ncc_log_level > 1) {
 					printk("pfn=0x%lx ncc_alloced=0x%x ncc_flags=0x%x\n", virt_to_pfn(skb), skb->ncc_alloced, skb->ncc_flags);
 				}
@@ -233,25 +316,30 @@ static int dump_slab_page(struct seq_file *m, struct list_head *head, int obj_si
 static void show_counters(struct seq_file *m) {
 	int i, j;
 	u64 total = 0;
+	u64 host_tot = 0;
+	u64 bridged_tot = 0;
 	for (i = 0; i < MIST_MOD_LAST; i++) {
 		for (j = 0; j < MIST_CAP_LAST; j++) {
 			if (j == 0) {
-				seq_printf(m, "\n%d] %s Total=%llu\n", i, module_itoa[i], n_stats.mist_counters[i][j]);
+				seq_printf(m, "\n%d] %s Total=%llu, bridged=%llu host=%llu\n", i, module_itoa[i], n_stats.mist_counters[i][j].cntr, n_stats.mist_counters[i][j].bridged, n_stats.mist_counters[i][j].host);
 			} else {
-				total += n_stats.mist_counters[i][j];
-				if (n_stats.mist_counters[i][j]) {
-					seq_printf(m, "%s Cap_Point%d_Total=%llu\n", module_itoa[i], j, n_stats.mist_counters[i][j]);
+				total += n_stats.mist_counters[i][j].cntr;
+				host_tot += n_stats.mist_counters[i][j].host;
+				bridged_tot += n_stats.mist_counters[i][j].bridged;
+				if (n_stats.mist_counters[i][j].cntr) {
+					seq_printf(m, "%s Cap_Point%d_Total=%llu bridged=%llu host=%llu\n", module_itoa[i], j, n_stats.mist_counters[i][j].cntr, n_stats.mist_counters[i][j].bridged, n_stats.mist_counters[i][j].host);
 				}
 			}
 		}
 	}
-	seq_printf(m, "Total_SKB_allocated=%llu\n", total);
+	seq_printf(m, "Total_SKB_allocated=%llu, bridged=%llu, host=%llu\n", total, bridged_tot, host_tot);
 	seq_printf(m, "\nLast Seen on\n");
 	for (i = 0; i < MIST_MOD_LAST; i++) {
-		if (n_stats.mist_lastseen_counters[i]) {
-			seq_printf(m, "%s=%llu\n", module_itoa[i], n_stats.mist_lastseen_counters[i]);
+		if (n_stats.mist_lastseen_counters[i].cntr) {
+			seq_printf(m, "%s=%llu, bridged=%llu, host=%llu\n", module_itoa[i], n_stats.mist_lastseen_counters[i].cntr, n_stats.mist_lastseen_counters[i].bridged, n_stats.mist_lastseen_counters[i].host);
 		}
 	}
+
 	if (n_stats.mist_cntr_err) {
 		seq_printf(m, "mist_cntr_err=%llu\n", n_stats.mist_cntr_err);
 	}
@@ -283,6 +371,7 @@ static void build_stats(struct seq_file *m) {
 static int ncc_dump_show(struct seq_file *m, void *arg) {
 
 	memset(&n_stats, 0x0, sizeof(ncc_stats_t));
+	ncc_logged_pkt_cnt = 0;
 	build_stats(NULL);
 
 	show_counters(m);
@@ -292,7 +381,20 @@ static int ncc_dump_show(struct seq_file *m, void *arg) {
 
 static int skb_dump_show(struct seq_file *m, void *arg) {
 	memset(&n_stats, 0x0, sizeof(ncc_stats_t));
+	ncc_dump_cmd = 1;
+	ncc_logged_pkt_cnt = 0;
 	build_stats(m);
+	ncc_dump_cmd = 0;
+	return 0;
+
+}
+
+static int skb_dump_single_show(struct seq_file *m, void *arg) {
+	memset(&n_stats, 0x0, sizeof(ncc_stats_t));
+	ncc_dump_cmd = 2;
+	ncc_logged_pkt_cnt = 0;
+	build_stats(m);
+	ncc_dump_cmd = 0;
 	return 0;
 
 }
@@ -304,6 +406,7 @@ static int __init ncc_init(void) {
 		proc_create_single(ncc_dump, ncc_proc_dir, NULL);
 		proc_create_single(config, ncc_proc_dir, config_wr);
 		proc_create_single(skb_dump, ncc_proc_dir, NULL);
+		proc_create_single(skb_dump_single, ncc_proc_dir, NULL);
 	}
 	if (skbuff_head_cache) {
 		printk("NCC[%s] slab=%px %s size=%d object_size=%d\n", __func__, skbuff_head_cache, skbuff_head_cache->name, skbuff_head_cache->size, skbuff_head_cache->object_size);
