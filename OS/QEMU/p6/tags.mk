5ae3226c9b2d3b261ceb8bcb733f464157f20520
Muhammed Nishad Chengalan Chalipurath <mnishadc@juniper.net>
2024-04-25 20:33:38 -0700

    Raspberry pi 4b bare metal programming.

--- OS/QEMU/p6/tags.mk
+++ OS/QEMU/p6/tags.mk
@@ -9,13 +9,15 @@ endif
 ifeq (${ARCH},arm64)
 TARGET:=kernel8
 MACHINE:=raspi3
-TOOLCHAIN:=aarch64
+TOOLCHAIN:=arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf
+TOOLCHAIN_VER:=13.2.1
 CROSS_COMPILE:=aarch64-none-elf-
 endif
 ifeq (${ARCH},arm32)
 TARGET:=kernel
 MACHINE:=vexpress-a9
 TOOLCHAIN:=arm
+TOOLCHAIN_VER:=12.2.0
 CROSS_COMPILE:=arm-none-eabi-
 endif
 
@@ -29,16 +31,18 @@ endif
 ifeq (${MACHINE},raspi3)
 DEFINES:=-DRASPI3 -DARM64 -DAARCH64
 #mkfifo /tmp/guest.in /tmp/guest.out
+QEMU:=~/Documents/qemu/build/qemu-system-aarch64
 #QEMU_CMD:=qemu-system-aarch64 -M ${MACHINE} -smp 4 -m 1024M  -no-reboot -nographic -serial pipe:/tmp/guest.out -kernel ${TARGET}.bin 
-QEMU_CMD:=qemu-system-aarch64 -M ${MACHINE} -smp 4 -m 1024M  -no-reboot -nographic -kernel ${TARGET}.bin 
+QEMU_CMD:=${QEMU} -M ${MACHINE} -smp 4 -m 1024M  -no-reboot -nographic -kernel ${TARGET}.bin 
 #QEMU_CMD:=qemu-system-aarch64 -M ${MACHINE} -smp 4 -m 1024M -no-reboot ${SERIAL_CONSOLE} -kernel ${TARGET}.bin 
-QEMU_GDB_CMD:=qemu-system-aarch64 -M ${MACHINE} -smp 4 -m 1024M -no-reboot ${SERIAL_CONSOLE} -kernel ${TARGET}.bin -S -gdb tcp::3333
+QEMU_GDB_CMD:=${QEMU} -M ${MACHINE} -smp 4 -m 1024M -no-reboot ${SERIAL_CONSOLE} -kernel ${TARGET}.bin -S -gdb tcp::3333
 endif
 
 ifeq (${MACHINE},vexpress-a9)
 DEFINES:=-DVEXP_A9 -DARM32 -DARM
-QEMU_GDB_CMD:=qemu-system-arm -M ${MACHINE} -m 512M -no-reboot -nographic -kernel ${TARGET}.bin -S -gdb tcp::3333
-QEMU_CMD:=qemu-system-arm -M ${MACHINE} -m 512M -no-reboot -nographic -kernel ${TARGET}.bin
+QEMU:=~/Documents/qemu/build/qemu-system-arm
+QEMU_GDB_CMD:=${QEMU} -M ${MACHINE} -m 512M -no-reboot -nographic -kernel ${TARGET}.bin -S -gdb tcp::3333
+QEMU_CMD:=${QEMU} -M ${MACHINE} -m 512M -no-reboot -nographic -kernel ${TARGET}.bin
 endif
 
 TOOLCHAIN_ROOT:=/home/nishad/Documents//toolchain/
@@ -67,14 +71,14 @@ CFLAGS= -O0 -ggdb3 -g3 -Wall -MMD ${DEFINES} ${INCLUDES} -nostdlib -nostartfiles
 
 ifeq (${ARCH},arm64)
 LDFLAGS:= -L${TOOLCHAIN_PATH}/aarch64-none-elf/lib/ -lc 
-LDFLAGS+= -L${TOOLCHAIN_PATH}/lib/gcc/aarch64-none-elf/11.2.1/ -lgcc
+LDFLAGS+= -L${TOOLCHAIN_PATH}/lib/gcc/aarch64-none-elf/${TOOLCHAIN_VER}/ -lgcc
 LDFLAGS+= -L${TOOLCHAIN_PATH}/aarch64-none-elf/lib/ -lm
 LDSCRIPT:=link64.ld
 endif
 
 ifeq (${ARCH},arm32)
 LDFLAGS:= -L${TOOLCHAIN_PATH}/arm-none-eabi/lib/ -lc
-LDFLAGS+= -L${TOOLCHAIN_PATH}/lib/gcc/arm-none-eabi/12.2.0/ -lgcc
+LDFLAGS+= -L${TOOLCHAIN_PATH}/lib/gcc/arm-none-eabi/${TOOLCHAIN_VER}/ -lgcc
 LDFLAGS+= -L${TOOLCHAIN_PATH}/arm-none-eabi/lib/ -lm
 LDSCRIPT:=link32.ld
 endif
