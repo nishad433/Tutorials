46a77f59c29e9c679808cf1b0049582d9080f351
Muhammed Nishad Chengalan Chalipurath <mnishadc@juniper.net>
2024-05-01 19:15:28 -0700

    reorganised the directories

--- OS/QEMU/raspi4b/p1/src/kernel/utils.c
+++ OS/QEMU/raspi4b/p1/src/kernel/utils.c
@@ -1,27 +1,6 @@
-#include <stddef.h>
-#include <stdint.h>
+#include <utils.h>
 
 extern uint64_t _text_end, _bss_start, _bss_end, _data_start, _data_end;
-
-void *memset(void *ptr, int c, size_t n) {
-  for (int i = 0; i < n; i++) {
-    ((char *)ptr)[i] = c;
-  }
-  return ptr;
-}
-
-void *memcpy(void *dest, const void *src, size_t n) {
-  if (dest > src && (char *)dest < ((char *)src) + n) {
-    for (int i = n - 1; i >= 0; i--) {
-      ((char *)dest)[i] = ((char *)src)[i];
-    }
-  } else {
-    for (int i = 0; i < n; i++) {
-      ((char *)dest)[i] = ((char *)src)[i];
-    }
-  }
-  return dest;
-}
 
 void clear_bss() {
   memset((void *)&_bss_start, 0,
